cd3404150d444acb27be84704039875aed5c38c7
ericpkatz <ericpkatz@gmail.com>
2023-03-29 20:03:14 -0400

    add model and api route

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { conn } = require('./db');
+const { conn, Widget } = require('./db');
 
 app.use('/dist', express.static('dist'));
 app.use('/assets', express.static('assets'));
 app.use(express.json());
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/api/widgets', async(req, res, next)=> {
+  try {
+    res.send(await Widget.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 
 const port = process.env.PORT || 3000;
@@ -14,7 +23,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, async()=> {
   try {
     console.log(`listening on port ${port}`);
-    //await conn.sync({ force: true });
+    await conn.sync({ force: true });
+    await Promise.all(
+      ['foo', 'bar', 'bazz', 'quq'].map( name => Widget.create({ name }))
+    );
+    console.log('seeded');
   }
   catch(ex){
     console.log(ex);
